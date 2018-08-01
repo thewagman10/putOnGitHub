ddb094434d11c6878896bf4077dc29c2416d7e2b
Michael Wagner <thewagman@michaels-mbp-5.lan>
2018-08-01 15:48:40 -0500

    linked some view controllers and made a new storyboard

---
+++ iceberg/Controllers/AfterLogin.swift
@@ -0,0 +1,11 @@
+//
+//  AfterLogin.swift
+//  iceberg
+//
+//  Created by Michael Wagner on 8/1/18.
+//  Copyright © 2018 iceberg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
---
+++ iceberg/Controllers/AnotherViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AnotherViewController.swift
+//  iceberg
+//
+//  Created by Michael Wagner on 8/1/18.
+//  Copyright © 2018 iceberg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+
